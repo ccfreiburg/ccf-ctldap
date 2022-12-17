954afe43b1389bdae8edd488bb568bbdabe6a567
Frieder Schrempf <frieder@fris.de>
2022-12-17 16:50:04 +0100

    ctapi.js: Fix code style

--- src/ctapi.js
+++ src/ctapi.js
@@ -1,36 +1,33 @@
-const log = require('./logging');
-const c = require('./constants');
 const axios = require('axios').default;
+const log = require('./logging');
 
 exports.ChurchToolsError = (message) => {
-  err = new Error(message);
+  const err = new Error(message);
   log.error(message);
   err.name = 'ChurchToolsError';
   return err;
 };
 
 exports.ChurchToolsFatalError = (message) => {
-  err = new Error(message);
+  const err = new Error(message);
   log.error(message);
   err.name = 'ChurchToolsFatalError';
   return err;
 };
 
-uriTrailingSlash = (uri) => (uri.slice(-1) !== '/' ? `${uri}/` : uri);
-
 exports.result = (result, success, failed) => {
-  if (result && result.hasOwnProperty('data')) {
+  if (result && Object.prototype.hasOwnProperty.call(result, 'data')) {
     if (success) success(result);
     log.debug(JSON.stringify(result.data));
     return result.data;
-  } if (result && result.hasOwnProperty('status') && result.status === 'success') {
+  } if (result && Object.prototype.hasOwnProperty.call(result, 'status') && result.status === 'success') {
     if (success) success(result);
     log.debug(JSON.stringify(result.data));
     return result.data;
-  } if (result && result.hasOwnProperty('message')) {
+  } if (result && Object.prototype.hasOwnProperty.call(result, 'message')) {
     if (failed) failed();
     throw this.ChurchToolsError(result.message);
-  } else if (result && result.hasOwnProperty('status')
+  } else if (result && Object.prototype.hasOwnProperty.call(result, 'status')
     && [400, 401, 403, 404, 405, 500, 501, 502, 503, 504].includes(result.status)) {
     if (failed) failed();
     throw this.ChurchToolsError('No Session or connection problems');
